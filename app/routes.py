82b35cff1da35a048d2cbe20c7d244834bf92f57
Andre Burke <aburke.dev@gmail.com>
2022-12-04 23:03:36 -0700

    added front door lock and unlock

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 
-DWELO_BASE = "https://api.dwelo.com/v3/"
-LOGIN = "login"
+DWELO_BASE_URL = "https://api.dwelo.com/v3/"
+LOGIN = "login/"
+# TODO: replace with gateway id here, need to find out how to get the gateway id
 DEVICE_LIST = "device/?gatewayId=192542&limit=5000&offset=0"
-COMMAND = "device/deviceId/command/"
+COMMAND = "device/device_id/command/"
